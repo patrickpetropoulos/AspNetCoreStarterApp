72acf62f3f0afbc0f2b7d4da2e98ddb98fe0edee
Patrick <patrickpetropoulos@gmail.com>
2020-06-23 16:49:45 -0400

    Initial Commit

---
+++ AspNetCoreStarterAppAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreStarterAppAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreStarterAppAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
